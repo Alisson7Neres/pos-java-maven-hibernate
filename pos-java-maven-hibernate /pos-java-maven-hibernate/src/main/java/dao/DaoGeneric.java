23c677e35dc84570c17ab2c98fc69be8ad376596
Alisson Neres <53406391+Alisson7Neres@users.noreply.github.com>
2020-09-16 10:36:48 -0300

    Add files via upload

---
+++ pos-java-maven-hibernate /pos-java-maven-hibernate/src/main/java/dao/DaoGeneric.java
@@ -0,0 +1,62 @@
+package dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import posjavamavenhibernate.HibernateUtil;
+
+public class DaoGeneric<E> {
+
+	// EntityManager é a classe responsável por gerenciar o ciclo de vida das entidades
+	private EntityManager entityManager = HibernateUtil.getEntityManager();
+
+	public void salvar(E entidade) {
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+		entityManager.persist(entidade);
+		transaction.commit();
+	}
+	
+	public E updateMerge(E entidade) { // Salva ou atualiza
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+		E entidadeSalva = entityManager.merge(entidade);
+		transaction.commit();
+		
+		return entidadeSalva;
+	}
+	
+	public void deletarPorId(E entidade) {
+		Object id = HibernateUtil.getPrimaryKey(entidade);
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+		
+		entityManager.createNativeQuery("delete from "+ entidade.getClass().getSimpleName().toLowerCase()
+				+ " where id = " + id).executeUpdate(); // Faz o delete
+		transaction.commit(); // Grava a alteração no banco
+	}
+	
+	public E pesquisar(Long id, Class<E> entidade) {
+		E e = (E) entityManager.find(entidade,id);
+
+		return e;
+	}
+	
+	public List<E> listar(Class<E> entidade){
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+		
+		@SuppressWarnings("unchecked")
+		List<E> lista = entityManager.createQuery("from " + entidade.getName()).getResultList();
+		
+		transaction.commit();
+		return lista;
+	}
+	
+	public EntityManager getEntityManager() {
+		return entityManager;
+	}
+
+}
